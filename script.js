cadfa9876ffa4c97a02b9406bcd4b24c97e924e7
CoderHritik0 <hritiksanas47@gamil.com>
2024-01-12 11:20:57 +0530

    Rendering Classes Dynamically from JavaScript Object

---
+++ script.js
@@ -0,0 +1,378 @@
+let classInfo = [
+  {
+    OrgName: "Classcapades",
+    className: "BRP - Building Readers Programme (Grade 1)",
+    address: "Khar West, Mumbai",
+    ageGroupStart: 60,
+    ageGroupEnd: 84,
+    days: ["Tue", "Thu", "Fri"],
+    startDate: "Jun 13, 2023",
+    endDate: "Jun 15, 2024",
+    categories: ["Language", "Phonics"],
+    classType: "Weekly",
+    rating: 2,
+    img: "1.jpg",
+  },
+  {
+    OrgName: "The Lil Yogi's Club",
+    className: "YOGA DISCO TODDLERS",
+    address: "Colaba, Mumbai",
+    ageGroupStart: 18,
+    ageGroupEnd: 96,
+    days: ["Tue", "Thu"],
+    startDate: "Jun 13, 2023",
+    endDate: "Jun 12, 2024",
+    categories: ["Sports & Fitness"],
+    classType: "Monthly",
+    rating: 3,
+    img: "2.jpg",
+  },
+  {
+    OrgName: "Magic Beans",
+    className: "Magic Beans : The Wonderful World of Jolly Phonics LEVEL 1",
+    address: "Tardeo, Mumbai",
+    ageGroupStart: 30,
+    ageGroupEnd: 60,
+    days: ["Mon", "Sat"],
+    startDate: "Jun 12, 2023",
+    endDate: "Jun 16, 2024",
+    categories: ["Phonics"],
+    classType: "Sessions",
+    rating: 1,
+    img: "3.jpg",
+  },
+  {
+    OrgName: "Allrounder",
+    className: "Aerial Silk",
+    address: "Dadar, Mumbai",
+    ageGroupStart: 48,
+    ageGroupEnd: 216,
+    days: ["Sat", "Sun"],
+    startDate: "Jun 12, 2023",
+    endDate: "Jun 12, 2024",
+    categories: ["Sports & Fitness"],
+    classType: "Monthly",
+    rating: 5,
+    img: "4.jpg",
+  },
+  {
+    OrgName: "Magic Beans",
+    className: "Magic Beans Mommy And Me",
+    address: "Taedeo, Mumbai",
+    ageGroupStart: 10,
+    ageGroupEnd: 30,
+    days: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+    startDate: "Jun 12, 2023",
+    endDate: "Jun 19, 2024",
+    categories: ["Parent Toddler"],
+    classType: "Sessions",
+    rating: 3,
+    img: "5.jpg",
+  },
+  {
+    OrgName: "The Yoga House Studio & Cafe",
+    className: "SHADAJA - A MUSIC PROGRAMME IN BANDRA for Parents & Toddlers",
+    address: "Bandra West, Mumbai",
+    ageGroupStart: 6,
+    ageGroupEnd: 32,
+    days: ["Tue"],
+    startDate: "Jul 4, 2023",
+    endDate: "Jun 4, 2024",
+    categories: ["Dance"],
+    classType: "Course",
+    rating: 4,
+    img: "6.jpg",
+  },
+  {
+    OrgName: "Magic Beans",
+    className: "Magic Beans Sensory Play",
+    address: "Tardeo, Mumbai",
+    ageGroupStart: 12,
+    ageGroupEnd: 30,
+    days: ["Thu"],
+    startDate: "Jun 15, 2023",
+    endDate: "Jun 14, 2024",
+    categories: ["Art & Sensory"],
+    classType: "Monthly",
+    rating: 4,
+    img: "7.jpg",
+  },
+  {
+    OrgName: "Magic Beans",
+    className: "Magic Beans Yoga For Kids",
+    address: "Tardeo, Mumbai",
+    ageGroupStart: 36,
+    ageGroupEnd: 72,
+    days: ["Tue"],
+    startDate: "Jun 13, 2023",
+    endDate: "Jun 12, 2024",
+    categories: ["Sports & Fitness"],
+    classType: "Monthly",
+    rating: 3,
+    img: "8.jpg",
+  },
+  {
+    OrgName: "Magic Beans",
+    className: "Magic Beans - Parent Child RHYME & RHYTHM",
+    address: "Tardeo, Mumbai",
+    ageGroupStart: 18,
+    ageGroupEnd: 60,
+    days: ["Mon"],
+    startDate: "Jun 12, 2023",
+    endDate: "May 12, 2024",
+    categories: ["Music"],
+    classType: "Sessions",
+    rating: 2,
+    img: "9.jpg",
+  },
+  {
+    OrgName: "Little Kickers",
+    className: "Little Kickers Football",
+    address: "Bandra West, Mumbai",
+    ageGroupStart: 18,
+    ageGroupEnd: 60,
+    days: ["Tue", "Wed", "Thu", "Fri"],
+    startDate: "Jun 1, 2023",
+    endDate: "Jun 30, 2024",
+    categories: ["Sports & Fitness"],
+    classType: "Quarterly",
+    rating: 2,
+    img: "10.jpg",
+  },
+  {
+    OrgName: "Little Gym Of Bandra",
+    className: "Gymnastics Classes",
+    address: "Bandra West, Mumbai",
+    ageGroupStart: 19,
+    ageGroupEnd: 72,
+    days: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    startDate: "Jun 1, 2023",
+    endDate: "Jun 30, 2024",
+    categories: ["Sports & Fitness"],
+    classType: "Quarterly",
+    rating: 4,
+    img: "11.jpg",
+  },
+  {
+    OrgName: "Little Kickers",
+    className: "Little Kickers Football",
+    address: "Juhu, Mumbai",
+    ageGroupStart: 18,
+    ageGroupEnd: 60,
+    days: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+    startDate: "Jun 1, 2023",
+    endDate: "Jun 30, 2024",
+    categories: ["Sports & Fitness"],
+    classType: "Quarterly",
+    rating: 2,
+    img: "12.jpg",
+  },
+  {
+    OrgName: "Magic Beans",
+    className: "Magic Beans BRAIN BOOSTERS",
+    address: "Tardeo, Mumbai",
+    ageGroupStart: 30,
+    ageGroupEnd: 48,
+    days: ["Mon", "Wed", "Fri"],
+    startDate: "Jun 17, 2023",
+    endDate: "Jul 16, 2024",
+    categories: ["Language", "Phonics"],
+    classType: "Monthly",
+    rating: 5,
+    img: "13.jpg",
+  },
+  {
+    OrgName: "Allrounders",
+    className: "Drawing Classes",
+    address: "Dadar, Mumbai",
+    ageGroupStart: 36,
+    ageGroupEnd: 120,
+    days: ["Fri", "Sat"],
+    startDate: "Jul 1, 2023",
+    endDate: "Jun 30, 2024",
+    categories: ["Art & Sensory"],
+    classType: "Monthly",
+    rating: 3,
+    img: "14.jpg",
+  },
+  {
+    OrgName: "Allrounders",
+    className: "Keyboard Classes",
+    address: "Dadar, Mumbai",
+    ageGroupStart: 78,
+    ageGroupEnd: 168,
+    days: ["Wed"],
+    startDate: "Jul 1, 2023",
+    endDate: "Jun 30, 2024",
+    categories: ["Music"],
+    classType: "Monthly",
+    rating: 2,
+    img: "15.jpg",
+  },
+  {
+    OrgName: "Allrounders",
+    className: "Ballet Class",
+    address: "Dadar, Mumbai",
+    ageGroupStart: 48,
+    ageGroupEnd: 144,
+    days: ["Tue"],
+    startDate: "Jul 1, 2023",
+    endDate: "Jun 30, 2024",
+    categories: ["Dance"],
+    classType: "Monthly",
+    rating: 4,
+    img: "16.jpg",
+  },
+  {
+    OrgName: "Allrounders",
+    className: "Taekwondo Class",
+    address: "Dadar, Mumbai",
+    ageGroupStart: 60,
+    ageGroupEnd: 144,
+    days: ["Sat"],
+    startDate: "Jul 1, 2023",
+    endDate: "Jun 30, 2024",
+    categories: ["Sports & Fitness"],
+    classType: "Monthly",
+    rating: 3,
+    img: "17.jpg",
+  },
+  {
+    OrgName: "Allrounder",
+    className: "Bollywood Dance Class",
+    address: "Dadar, Mumbai",
+    ageGroupStart: 96,
+    ageGroupEnd: 108,
+    days: ["Mon", "Thu"],
+    startDate: "Jul 1, 2023",
+    endDate: "Jun 30, 2024",
+    categories: ["Dance"],
+    classType: "Monthly",
+    rating: 5,
+    img: "18.jpg",
+  },
+  {
+    OrgName: "Yoga With Anindita",
+    className: "Yoga With Anindita",
+    address: "Santacruz West, Mumbai",
+    ageGroupStart: 36,
+    ageGroupEnd: 84,
+    days: ["Sun"],
+    startDate: "Jul 1, 2023",
+    endDate: "Jun 30, 2024",
+    categories: ["Sun"],
+    classType: "Sports & Fitness",
+    rating: 4,
+    img: "19.jpg",
+  },
+  {
+    OrgName: "Gehna Mirchandani",
+    className: "Gehna's Jolly Classes",
+    address: "Bandra West, Mumbai",
+    ageGroupStart: 36,
+    ageGroupEnd: 60,
+    days: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    startDate: "Jul 1, 2023",
+    endDate: "Jun 30, 2024",
+    categories: ["Art & Sensory", "Phonics"],
+    classType: "Monthly",
+    rating: 0,
+    img: "20.jpg",
+  },
+];
+
+$(document).ready(function () {
+  updateList();
+});
+
+function updateList() {
+  const classContainer = document.getElementById("classes");
+  classInfo.forEach((element) => {
+    let OrgName = element.OrgName;
+    let className = element.className;
+    let address = element.address;
+    let ageGroupStart = element.ageGroupStart;
+    let ageGroupEnd = element.ageGroupEnd;
+    let days = element.days;
+    let startDate = element.startDate;
+    let endDate = element.endDate;
+    let categories = element.categories;
+    let classType = element.classType;
+    let rating = element.rating;
+    let img = element.img;
+
+    classContainer.innerHTML += `
+        <div class='card mb-3'>
+                <div class='row g-0'>
+                  <div class='col-lg-3 col-xl-2 align-items-center d-flex'>
+                    <img src='/src/${img}' class='img-fluid' alt='Class Thumbnail' />
+                  </div>
+                  <div class='col-lg-9 col-xl-8'>
+                    <div class='card-body'>
+                      <p class='class-name mb-1'><small class='text-body-secondary'>${OrgName}</small></p>
+                      <h5 class='card-title mb-2'>${className}</h5>
+                      <div class='row mb-2'>
+                        <div class='col-md-6'>
+                          <p class='card-text address mb-1'>${address}</p>
+                          <p class='card-text days'>${getDaysString(days)}</p>
+                        </div>
+                        <div class='col-md-6'>
+                          <p class='card-text mb-1'>Age: ${getAgeString(ageGroupStart)} - ${getAgeString(ageGroupEnd)}</p>
+                          <p class='card-text mb-0'>${startDate} - ${endDate}</p>
+                        </div>
+                      </div>
+                      <div class='class-category'>
+                        <span class='badge rounded-pill'>${categories[0]}</span>
+                        <span class='extra-category'>${getCategory(categories)}</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class='col-lg-12 col-xl-2'>
+                    <div class='card-body h-100 d-flex flex-column justify-content-between text-end'>
+                      <div class='rating'>${giveRating(rating)}</div>
+                      <div class='class-type'>
+                        <p>${classType}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        `;
+  });
+}
+
+function getDaysString(daysArray) {
+  let dayString = "";
+  if (daysArray.length === 1) {
+    return daysArray[0];
+  }
+  daysArray.forEach((element) => {
+    dayString += element + " ";
+  });
+  dayString = dayString.slice(0, -1);
+  return dayString;
+}
+
+function getAgeString(months) {
+  // let ageString = (start / 12 >= 1) ? (start % 12 === 0) ? (` - ${start/12}yrs`) :(` - ${Math.floor(start/12)}yrs ${start%12}Mo` ): ` - ${start%12}Mo`;
+  // ageString += (end / 12 >= 1) ? (end % 12 === 0) ? (` - ${end/12}yrs`) :(` - ${Math.floor(end/12)}yrs ${end%12}Mo` ): ` - ${end%12}Mo`;
+  let ageString = months / 12 >= 1 ? (months % 12 === 0 ? ` - ${months / 12}yrs` : ` - ${Math.floor(months / 12)}yrs ${months % 12}Mo`) : ` - ${months % 12}Mo`;
+  return ageString;
+}
+
+function getCategory(categories) {
+  let noOfCategories = categories.length > 1 ? `+${categories.length - 1}` : ``;
+  return noOfCategories;
+}
+
+function giveRating(stars) {
+  let starHtml = "";
+  for (let i = 1; i <= 5; i++) {
+    if (i <= stars) {
+      starHtml += `<i class='bi bi-star-fill'></i>`;
+    } else {
+      starHtml += `<i class='bi bi-star'></i>`;
+    }
+  }
+  return starHtml;
+}
